57f92196065c02ed079afce2d9dcb39b67dc4126
kagnara <kol.kagnara@gmail.com>
2019-02-21 21:34:09 +0700

    8:init slide show

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Frontend\SlideShowRepository;
 
 /**
  * Class HomeController.
  */
 class HomeController extends Controller
 {
+	public function __construct(SlideShowRepository $slide){
+		$this->slide = $slide;
+	}
     /**
      * @return \Illuminate\View\View
      */
     public function index()
     {
-        return view('menu.home');
+    	$list_slide = $this->slide->get();
+    	$data = compact('list_slide');
+        return view('menu.home',$data);
     }
 }
